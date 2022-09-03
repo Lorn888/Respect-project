00a8af153f5be3cbd3addbb7fb6c8a6f92d12c5b
Patryk <patrykokpara@hotmail.com>
2022-09-04 00:04:04 +0100

    Refactored make_make snippet method

--- lib/make_snippet.rb
+++ lib/make_snippet.rb
@@ -1,8 +1,7 @@
 
 def make_snippet(string)
-    divided = string.split(" ").length
     n = 5
-    if divided > 5 
+    if string.split(" ").length > 5
         return string.split[0...n].join(" ") + " ..."
     end
 end
